81a25b629226c3a87d278a7c2844f63e2a1e09ef
gerald324 <geraldo.bs.rodriguez@outlook.cl>
2023-08-31 23:26:38 -0400

    Comentarios añadidos
    Se añadieron comentarios descriptivos del código

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,10 +1,15 @@
 if __name__ == "__main__":
+    # Itera sobre los valores del 1 al 100
     for i in range(1, 101):
+        # Revisa si es divisible por 3 y 5
         if i % 3 == 0 and i % 5 == 0:
             i = "FizzBuzz"
+        # Revisa si es divisible por 3
         elif i % 3 == 0:
             i = "Fizz"
+        # Revisa si es divisible por 5
         elif i % 5 == 0:
             i = "Buzz"
+        # Imprime el valor correspondiente
         print(i)
         
--- SPW.py
+++ SPW.py
@@ -1,4 +1,9 @@
-def Reemplazo(linea):
+import sys
+
+def reemplazo(linea):
+    """
+    Reemplaza el primer asterisco (*) de la cadena recibida por 0 y 1
+    """
     x = 0
     for i in linea:
         if i == "*":
@@ -11,24 +16,30 @@ def Reemplazo(linea):
 
 if __name__ == "__main__":
     
-    import sys
-
+    # Set de valores permitidos
     permitidos = set("1" + "0" + "*")
     
+    # Reconoce si el valor fue ingresado por consola o por input
     if len(sys.argv) == 1:
         val = str(input("Ingrese Valor:"))
     else:
         val = sys.argv[1]
     
+    # Transforma el valor ingresado en un set para validarlo
     if set(val) <= permitidos:
-        arreglo = [val]
-        while "*" in arreglo[0]:
+        lista = [val]
+        # Por cada iteracion, valida que el primer elemento de la lista contenga un asterisco
+        while "*" in lista[0]:
             at = []
-            for a in arreglo:
-                lin1, lin2 = Reemplazo(a)
+            # Por cada elemento de la lista, reemplaza el primer asterisco encontrado
+            # y añade las dos posibles soliciones a la lista
+            for a in lista:
+                lin1, lin2 = reemplazo(a)
                 at.append(lin1)
                 at.append(lin2)
-            arreglo = at
-        print (arreglo)
+            # Reemplaza la lista anterior por la solucion de la iteracion actual
+            lista = at
+        # Imprime la lista con la solucion final 
+        print (lista)
     else:
         print("Valor no valido, solo pueden haber 0, 1 y *")
